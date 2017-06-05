0c43119a92d43f9ecca7d12e51b1890e001f9398
poojapauskar <git.poojapauskar@gmail.com>
2017-06-05 18:14:07 +0530

    importing data

---
+++ index.php
@@ -0,0 +1,13 @@
+<form enctype="multipart/form-data" action="upload.php" method="post" >
+  
+  <!-- <label style="margin-left:2%" class="form-label span3" for="file">Import Students</label><br><br>
+   -->
+   <input style="margin-left:1%;margin-top:2%" type="file" name="file" id="file" required />
+  
+  
+  <br><br>
+  <button class="btn btn-success" style="color:white;" type="submit">
+    Import
+    </button>
+
+</form>
